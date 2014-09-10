25deb88a36df0c07f31e62209e53542b900011a4
admin <example@email.com>
2014-09-10 16:58:34 +0400

    проба вывода информации по постам в blog

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,5 @@ from blog import views
 
 urlpatterns = patterns('',
     url(r'^$', views.main, name='main'),
+	url(r'^(?P<post_id>\d+)/$', views.detail, name='detail'),
 )
--- blog/views.py
+++ blog/views.py
@@ -2,13 +2,27 @@ from django.shortcuts import render
 
 # Create your views here.
 
-from django.http import HttpResponse
+from blog.models import Post
 
+from django.http import HttpResponse
 from django.template import RequestContext, loader
+from django.http import Http404
+
 
 def main(request):
+    full_post_list = Post.objects.order_by('-pub_date')[:3]
+
     template = loader.get_template('blog/index.html')
-    
+
     return render(request, 'blog/index.html', {
+		'full_post_list': full_post_list,
 		'error_message': "Something gone wrong.",
 		})
+
+
+def detail(request, post_id):
+    try:
+        post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    return render(request, 'blog/detail.html', {'post': post, })
