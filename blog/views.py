e7f7862e0255da4fe2bdd3454972104b6a959cec
admin <example@email.com>
2014-09-10 16:45:27 +0400

    добавили teamplate для blog

--- blog/views.py
+++ blog/views.py
@@ -4,5 +4,11 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.template import RequestContext, loader
+
 def main(request):
-	return HttpResponse("Hello, world. You're at the blog index.")
+    template = loader.get_template('blog/index.html')
+    
+    return render(request, 'blog/index.html', {
+		'error_message': "Something gone wrong.",
+		})
