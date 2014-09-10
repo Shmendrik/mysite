32914ea5364b9d4e9a2f3fcb796610b8f7f7eab1
admin <example@email.com>
2014-09-10 16:38:23 +0400

    прописали url до приложения

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,6 +9,6 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    # url(r'^$', views.main, name='main'),
+	url(r'^blog/', include('blog.urls')),
     
 )
