0921ca79e8d739f52a04c059df5c1a627495f9d7
admin <example@email.com>
2014-09-10 16:19:44 +0400

    создали модель блога

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+import datetime
+
+class Post(models.Model):
+	# title post
+    title = models.CharField(max_length=60)
+    # body post
+    text = models.TextField()
+    # publication date
+    pub_date = models.DateTimeField('date published')
+
+    def __unicode__(self):
+        return self.title
