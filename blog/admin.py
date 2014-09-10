4429a611082666949a8efb8fab132e2b2c4a8764
admin <example@email.com>
2014-09-10 16:35:20 +0400

    добавили blog в админку

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from blog.models import Post
+
+class PostAdmin(admin.ModelAdmin):
+
+    fieldsets = [
+        (None,               {'fields': ['title', 'text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+
+    list_display = ('title', 'pub_date', 'text')
+    list_filter = ['pub_date']	
+
+
+admin.site.register(Post, PostAdmin)
